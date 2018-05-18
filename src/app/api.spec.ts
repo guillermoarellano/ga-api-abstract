e3443f0e968f9437e664ddc417dd8f63fead7e23
Guillermo Arellano <guillermo.arellanojr@gmail.com>
2018-05-18 17:33:24 -0500

    Unit tests for appending the query parameters to the url.

--- src/app/api.spec.ts
+++ src/app/api.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { API, Endpoint } from './api';
+import { API, Endpoint, Parameter } from './api';
 
 beforeEach(() => {
     TestBed.configureTestingModule({
@@ -39,27 +39,58 @@ describe('API', () => {
  });
 
  it('should add a limit to the url', () => {
-     const url = '';
+     let url = '';
+
+     const params: Parameter[] = [['limit', 10]];
+     const api = new API();
+     url = api.get(Endpoint.items, params);
+
      expect(url).toBe('/items?limit=10');
  });
 
  it('should add an offset to the url', () => {
-     const url = '';
+     let url = '';
+
+     const params: Parameter[] = [['offset', 10]];
+     const api = new API();
+     url = api.get(Endpoint.items, params);
+
      expect(url).toBe('/items?offset=10');
  });
 
  it('should add a greater than or equal to filter to the url', () => {
-     const url = '';
+     let url = '';
+
+     const params: Parameter[] = [['price_gte', 100]];
+     const api = new API();
+     url = api.get(Endpoint.items, params);
+
      expect(url).toBe('/items?price_gte=100');
  });
 
  it('should add a less than filter to the url', () => {
-     const url = '';
+     let url = '';
+
+     const params: Parameter[] = [['price_lt', 1000]];
+     const api = new API();
+     url = api.get(Endpoint.items, params);
+
      expect(url).toBe('/items?price_lt=1000');
  });
 
  it('should do all the things!', () => {
-     const url = '';
+     let url = '';
+
+     const params: Parameter[] = [
+         ['price_gte', 100],
+         ['price_lt', 1000],
+         ['limit', 10],
+         ['offset', 20],
+     ];
+
+     const api = new API();
+     url = api.get(Endpoint.items, params);
+
      expect(url).toBe('/items?price_gte=100&price_lt=1000&limit=10&offset=20');
  });
 });
