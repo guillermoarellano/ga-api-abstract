a4080919cc6ca4aeb409b7d31e4e5e75e30bfd9a
Guillermo Arellano <guillermo.arellanojr@gmail.com>
2018-05-18 17:12:38 -0500

    Implemented base url endpoints enforcing proper selection using Enum

--- src/app/api.ts
+++ src/app/api.ts
@@ -1,14 +1,26 @@
-enum Endpoint {
+import { Injectable } from '@angular/core';
+
+export enum Endpoint {
   items,
   customers,
   employees
 }
 
-type Parameter = [string, number];
+export type Parameter = [
+  string,
+  number
+];
 
+
+@Injectable()
 export class API {
 
-  get(endpoint: Endpoint, parameters?: Parameter[]) {
+  get(endpoint: Endpoint, parameters?: Parameter[]): string {
+    let url = '/';
 
+    url += Endpoint[endpoint];
+
+
+    return url;
   }
 }
